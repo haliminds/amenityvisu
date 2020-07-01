20e47711c0f8dff9991fdd239458c9c4474c3b20
haliminds <hali.minds38@gmail.com>
2020-07-01 16:53:28 +0200

    refactoring + delete amenity with private access

--- assets/js/amenityVisu.js
+++ assets/js/amenityVisu.js
@@ -63,36 +63,6 @@ function computePolygonArea(polygon) {
 
 
 /**
- * Get a city name from a latitude and a longitude
- * @param  {float} lat input latitude
- * @param  {float} lon input longitude
- * @return {[type]} json response of nominatim api
- */
-async function getCityByLatLng(lat, lon) {
-  // appelle Nominatim pour savoir dans quelle commune on se trouve
-  // On fait un 1er appel à du reverse et/ou un appel normal pour avoir l'osm_id et le geojson si le premier appel merde
-  const nominatiUrl = 'https://nominatim.openstreetmap.org/reverse?lat=' + lat + '&lon=' + lon + '&polygon_geojson=1&format=geojson&zoom=10';
-  let nominatimResp = await fetch(nominatiUrl);
-  let nominatimGeoJson = await nominatimResp.json(); // read response body and parse as JSON
-  // Si ça pointe sur un ensemble de type Point, y'a un souci
-  // @TODO regarder si il faut d'autres verifs !!
-  let cityname = nominatimGeoJson.features[0].properties.address.city || nominatimGeoJson.features[0].properties.address.municipality;
-  // si ça pointe n'import où, ben tant pis !
-  if (cityname == undefined) {
-    return null;
-  }
-  // sinon, on cherche les infos sur la ville pointee
-  if (nominatimGeoJson.features[0].geometry.type == "Point") {
-    const urlCity = 'https://nominatim.openstreetmap.org/search.php?city=' + cityname + '&polygon_geojson=1&format=geojson&addressdetails=1';
-    let response_city = await fetch(urlCity);
-    nominatimGeoJson = await response_city.json();
-  }
-
-  return nominatimGeoJson;
-}
-
-
-/**
  * [computeVoronoi description]
  * @param  {[type]} amenity [description]
  * @return {[type]}         [description]
@@ -109,17 +79,8 @@ async function computeVoronoi(amenity) {
   await removeMarkers(map, "voronoi");
   await removeMarkers(map, "amenity_pt");
 
-  // Recherche des elemnts dans cette zone
-  // 3600000000 : on ajoute pour avoir la "relation" correpondante
-  const area_id = 3600000000 + parseInt(cityGeoJson.features[0].properties.osm_id, 10);
-  let overpassAmenityList = '';
-  elemDescr[amenity]["code"].forEach(elem => overpassAmenityList += 'node[' + elem + '](area.searchArea);');
-  const overpassApiUrl = 'https://lz4.overpass-api.de/api/interpreter?data=[out:json];area(' + area_id + ')->.searchArea;' + overpassAmenityList + 'out;';
-  let responseOverpass = await fetch(overpassApiUrl);
-  let osmDataAsJson = await responseOverpass.json(); // read response body and parse as JSON
-
-  // tranformation de la reponse en geojson
-  let elementData = osmtogeojson(osmDataAsJson);
+  // Get all elements of city with OverPassTurbo
+  let elementData = await getAmenityByOverPass(cityGeoJson, amenity);
 
   // chargement des points et transformation du tableau en objet
   var points = [];
---
+++ assets/js/externAPI.js
@@ -0,0 +1,57 @@
+
+const OVERPASSURL = 'https://lz4.overpass-api.de/api/interpreter?';
+const NOMINATIM = 'https://nominatim.openstreetmap.org/';
+const NOMINATIMREVERSEURL = NOMINATIM + 'reverse?'
+const NOMINATIMSEARCHURL = NOMINATIM + 'search.php?'
+
+/**
+ * Recherche les elements de type amenityType dans la ville cityGeoJson
+ * @param  {[type]} cityGeoJson [description]
+ * @param  {[type]} amenityType [description]
+ * @return {[type]}         [description]
+ */
+async function getAmenityByOverPass(cityGeoJson, amenityType) {
+
+  // Recherche des elements dans cette zone
+  // 3600000000 : on ajoute pour avoir la "relation" correpondante
+  const area_id = 3600000000 + parseInt(cityGeoJson.features[0].properties.osm_id, 10);
+  let overpassAmenityList = '';
+  elemDescr[amenityType]["code"].forEach(elem => overpassAmenityList += 'node[' + elem + ']["access"!~"private"](area.searchArea);');
+  const overpassApiUrl = OVERPASSURL + 'data=[out:json];area(' + area_id + ')->.searchArea;' + overpassAmenityList + 'out;';
+  let responseOverpass = await fetch(overpassApiUrl);
+  let osmDataAsJson = await responseOverpass.json(); // read response body and parse as JSON
+
+  // tranformation de la reponse en geojson
+  return osmtogeojson(osmDataAsJson);
+}
+
+
+
+/**
+ * Get a city name from a latitude and a longitude
+ * @param  {float} lat input latitude
+ * @param  {float} lon input longitude
+ * @return {[type]} json response of nominatim api
+ */
+async function getCityByLatLng(lat, lon) {
+  // appelle Nominatim pour savoir dans quelle commune on se trouve
+  // On fait un 1er appel à du reverse et/ou un appel normal pour avoir l'osm_id et le geojson si le premier appel merde
+  const nominatiUrl = NOMINATIMREVERSEURL + 'lat=' + lat + '&lon=' + lon + '&polygon_geojson=1&format=geojson&zoom=10';
+  let nominatimResp = await fetch(nominatiUrl);
+  let nominatimGeoJson = await nominatimResp.json(); // read response body and parse as JSON
+  // Si ça pointe sur un ensemble de type Point, y'a un souci
+  // @TODO regarder si il faut d'autres verifs !!
+  let cityname = nominatimGeoJson.features[0].properties.address.city || nominatimGeoJson.features[0].properties.address.municipality;
+  // si ça pointe n'import où, ben tant pis !
+  if (cityname == undefined) {
+    return null;
+  }
+  // sinon, on cherche les infos sur la ville pointee
+  if (nominatimGeoJson.features[0].geometry.type == "Point") {
+    const urlCity = NOMINATIMSEARCHURL + 'city=' + cityname + '&polygon_geojson=1&format=geojson&addressdetails=1';
+    let response_city = await fetch(urlCity);
+    nominatimGeoJson = await response_city.json();
+  }
+
+  return nominatimGeoJson;
+}
