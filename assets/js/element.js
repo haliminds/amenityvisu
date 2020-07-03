fdc761910099f28e7d3236dd91cc7c4149e1e3e2
haliminds <hali.minds38@gmail.com>
2020-07-03 17:31:09 +0200

    New menu + add svg of amenity

--- assets/js/element.js
+++ assets/js/element.js
@@ -1,5 +1,5 @@
 const elemDescr = {
-  "bicyleParking": {
+  "bicycleparking": {
     "descr": "Arceau à vélos",
     "code": ["amenity=bicycle_parking"]
   },
@@ -11,15 +11,10 @@ const elemDescr = {
     "descr": "Toilette",
     "code": ["amenity=toilets"]
   },
-  "wasteBasket": {
+  "wastebasket": {
     "descr": "Poubelle",
     "code": ["amenity=waste_basket"]
-  },
-	"surveillance" : {
-		"descr": "Caméra de surveillance",
-		"code": ["man_made=surveillance"]
-	}
-
+  }
 };
 
 
@@ -50,10 +45,18 @@ function createSideBar() {
   div_content_amenity.id = id_amenity;
   div_content_amenity.innerHTML += '<h1 class="leaflet-sidebar-header">Points d\'intérêt<span class="leaflet-sidebar-close"><i class="fa fa-caret-left"></i></span></h1><br>';
 
+  /*
   let amenity_option = '';
-  let checked = 'checked';
   for (let amenity in elemDescr) {
     amenity_option += '<input type="radio" id='+amenity+' name="amenity-radio" value='+amenity+' onchange="computeVoronoi(this.value);" '+checked+'><label for='+amenity+'>'+elemDescr[amenity].descr+'</label><br>'
+    checked = '';
+  }
+  */
+
+  let checked = 'checked';
+  let amenity_option = '<div class="amenitymatrix">';
+  for (let amenity in elemDescr) {
+    amenity_option += '<input type="radio" id='+amenity+' name="amenity-radio" value='+amenity+' onchange="computeVoronoi(this.value);" '+ checked+'><label for='+amenity+' class="singleamenity"><div class="'+amenity+'-img"></div>'+elemDescr[amenity].descr+'</label>';
     checked = '';
   }
 
