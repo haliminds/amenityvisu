8ce71b08784166bdb33522934c10bcce72527b48
Pierre Adam <55988747+haliminds@users.noreply.github.com>
2022-02-21 00:14:01 +0100

    [DEV] forget to convert to json

--- assets/js/externAPI.js
+++ assets/js/externAPI.js
@@ -40,14 +40,15 @@ async function getAmenityByOverPass(cityGeoJson, amenityType) {
 async function getCityByLatLng(lat, lon) {
   // appelle BigSataCloud pour savoir dans quelle commune on se trouve
   const requestiUrl = `${BIGDATACLOUD}latitude=${lat}&longitude=${lon}&localityLanguage=fr`;
-  let requestResp = await fetch(requestiUrl);
+  const requestResp = await fetch(requestiUrl);
+  const requestJson = await requestResp.json(); // read response body and parse as JSON
+ 
   // recuperation de la commune
   let cityname = requestResp.city.length>0 ? requestResp.city : requestResp.locality 
   // si ça pointe n'import où, ben tant pis !
   if (cityname == undefined) {
     return null;
   }
-  console.log(cityname)
   // appelle nominatim pour avoir le geojson à partir de la ville (pb avec le reverse)
   const urlCity = `${NOMINATIMSEARCHURL}city=${cityname}&polygon_geojson=1&format=geojson&addressdetails=1`;
   let response_city = await fetch(urlCity);
